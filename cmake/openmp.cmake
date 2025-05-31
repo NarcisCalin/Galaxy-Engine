991bf89078a8d3db7fdac3612239b2181b49f1aa
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2025-05-31 14:59:59 +0100

    Add janky fix for non-VC/CL clang on Windows with OpenMP

--- cmake/openmp.cmake
+++ cmake/openmp.cmake
@@ -1,14 +1,17 @@
 if(${CMAKE_VERSION} LESS 3.30 AND ${CMAKE_CXX_COMPILER_FRONTEND_VARIANT} STREQUAL MSVC)
 	add_library(openmp INTERFACE)
 
-	target_compile_options(openmp INTERFACE "-openmp:llvm")
+	target_compile_options(openmp INTERFACE -openmp:llvm)
 	target_link_libraries(openmp INTERFACE libomp)
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL Clang AND NOT ${CMAKE_CXX_COMPILER_FRONTEND_VARIANT} STREQUAL MSVC)
+	# Non-VC non-CL clang on Windows just does not want to cooperate with FindOpenMP.
+	add_library(openmp INTERFACE)
 
-	if(${CMAKE_CXX_COMPILER_ID} STREQUAL CLANG)
-	endif()
+	target_compile_options(openmp INTERFACE -fopenmp=libomp)
+	target_link_options(openmp INTERFACE -fopenmp=libomp)
 else()
 	set(OpenMP_RUNTIME_MSVC llvm)
-	find_package(OpenMP REQUIRED)
+	find_package(OpenMP REQUIRED COMPONENTS CXX)
 
 	add_library(openmp INTERFACE)
 	target_link_libraries(openmp INTERFACE OpenMP::OpenMP_CXX)
