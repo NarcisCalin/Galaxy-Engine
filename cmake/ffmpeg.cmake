e7843a525cd74641d26ad7a935abd3f4721f3e93
NarcisCalin <silenttoaster7@gmail.com>
2026-01-22 00:05:23 +0100

    Fixed project build files to work with visual studio

--- cmake/ffmpeg.cmake
+++ cmake/ffmpeg.cmake
@@ -1,18 +1,28 @@
 set(FFMPEG_URL_BASE https://github.com/BtbN/FFmpeg-Builds/releases/download/latest)
 
-if(LINUX)
-	set(FFMPEG_ARCHIVE ffmpeg-master-latest-linux64-lgpl-shared.tar.xz)
-elseif(WIN32)
-	set(FFMPEG_ARCHIVE ffmpeg-master-latest-win64-lgpl-shared.zip)
+if(WIN32)
+    set(FFMPEG_ARCHIVE ffmpeg-master-latest-win64-lgpl-shared.zip)
+else()
+    set(FFMPEG_ARCHIVE ffmpeg-master-latest-linux64-lgpl-shared.tar.xz)
 endif()
 
-FetchContent_Declare(ffmpeg-fetch URL ${FFMPEG_URL_BASE}/${FFMPEG_ARCHIVE})
+FetchContent_Declare(
+    ffmpeg-fetch
+    URL ${FFMPEG_URL_BASE}/${FFMPEG_ARCHIVE}
+)
+
 FetchContent_MakeAvailable(ffmpeg-fetch)
 
 add_library(ffmpeg INTERFACE)
 
-target_include_directories(ffmpeg INTERFACE ${ffmpeg-fetch_SOURCE_DIR}/include)
-
+target_include_directories(ffmpeg SYSTEM INTERFACE ${ffmpeg-fetch_SOURCE_DIR}/include)
 target_link_directories(ffmpeg INTERFACE ${ffmpeg-fetch_SOURCE_DIR}/lib)
 target_link_directories(ffmpeg INTERFACE ${ffmpeg-fetch_SOURCE_DIR}/bin)
-target_link_libraries(ffmpeg INTERFACE avcodec avformat avutil swscale swresample)
+
+target_link_libraries(ffmpeg INTERFACE 
+    avcodec 
+    avformat 
+    avutil 
+    swscale 
+    swresample
+)
--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -1,17 +1,22 @@
-set(IMGUI_VERSION v1.91.9b)
-set(IMGUI_URL_BASE https://github.com/ocornut/imgui/archive/refs/tags)
-
-FetchContent_Declare(imgui-fetch URL ${IMGUI_URL_BASE}/${IMGUI_VERSION}/v1.91.9b.zip)
+FetchContent_Declare(
+    imgui-fetch
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG        docking
+)
 FetchContent_MakeAvailable(imgui-fetch)
 
-set(IMPLOT_URL https://github.com/epezent/implot/archive/refs/heads/master.zip)
-
-FetchContent_Declare(implot-fetch URL ${IMPLOT_URL})
+FetchContent_Declare(
+    implot-fetch
+    GIT_REPOSITORY https://github.com/epezent/implot.git
+    GIT_TAG        master
+)
 FetchContent_MakeAvailable(implot-fetch)
 
-set(RLGUI_URL https://github.com/raylib-extras/rlImGui/archive/refs/heads/main.zip)
-
-FetchContent_Declare(rlgui-fetch URL ${RLGUI_URL})
+FetchContent_Declare(
+    rlgui-fetch
+    GIT_REPOSITORY https://github.com/raylib-extras/rlImGui.git
+    GIT_TAG        main
+)
 FetchContent_MakeAvailable(rlgui-fetch)
 
 add_library(imgui STATIC)
@@ -20,18 +25,21 @@ target_include_directories(imgui PUBLIC ${imgui-fetch_SOURCE_DIR})
 target_include_directories(imgui PUBLIC ${implot-fetch_SOURCE_DIR})
 target_include_directories(imgui PUBLIC ${rlgui-fetch_SOURCE_DIR})
 
-target_sources(
-	imgui PRIVATE
-	${imgui-fetch_SOURCE_DIR}/imgui.cpp
-	${imgui-fetch_SOURCE_DIR}/imgui_tables.cpp
-	${imgui-fetch_SOURCE_DIR}/imgui_widgets.cpp
-	${imgui-fetch_SOURCE_DIR}/imgui_draw.cpp)
+target_sources(imgui PRIVATE 
+    ${imgui-fetch_SOURCE_DIR}/imgui.cpp
+    ${imgui-fetch_SOURCE_DIR}/imgui_tables.cpp
+    ${imgui-fetch_SOURCE_DIR}/imgui_widgets.cpp
+    ${imgui-fetch_SOURCE_DIR}/imgui_draw.cpp
+    ${imgui-fetch_SOURCE_DIR}/imgui_demo.cpp
+)
 
-target_sources(
-	imgui PRIVATE
-	${implot-fetch_SOURCE_DIR}/implot.cpp
-	${implot-fetch_SOURCE_DIR}/implot_items.cpp)
+target_sources(imgui PRIVATE 
+    ${implot-fetch_SOURCE_DIR}/implot.cpp
+    ${implot-fetch_SOURCE_DIR}/implot_items.cpp
+)
 
-target_sources(imgui PRIVATE ${rlgui-fetch_SOURCE_DIR}/rlImGui.cpp)
+target_sources(imgui PRIVATE 
+    ${rlgui-fetch_SOURCE_DIR}/rlImGui.cpp
+)
 
 target_link_libraries(imgui PUBLIC raylib-lib)
